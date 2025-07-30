945c6c4418fbb68ed798abb6824e79526b4f14e0
hezronokwach <hezronokwach@gmail.com>
2025-07-30 23:39:58 +0300

    Remove contracts workspace and temporarily disable linting

---
+++ backend/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'error',
+    'no-console': 'warn',
+    'no-debugger': 'error',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        'no-console': 'off',
+      },
+    },
+  ],
+};
