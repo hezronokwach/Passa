7283857c91dcd06619c68fcbc24226250dcb8880
oduortoni <oduortoni@gmail.com>
2025-08-01 17:06:12 +0300

    feat: implement a simple Dashboard
    once a user is registered, we need a place to redirect them to

---
+++ frontend/src/pages/DashboardPage.tsx
@@ -0,0 +1,11 @@
+
+const DashboardPage = () => {
+  return (
+    <div className="min-h-screen p-8 bg-blue-900 text-white">
+      <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+      <p>Welcome to your dashboard!</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
